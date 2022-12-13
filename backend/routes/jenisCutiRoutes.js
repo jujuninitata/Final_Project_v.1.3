99b92b0b7357975edabdd4d617e1c4f3bfdfb1bb
suangga2003 <suangga2003@yahoo.com>
2022-12-13 16:35:06 +0700

    remove folder backend

--- backend/routes/jenisCutiRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getAllJenisCuti,getJenisCutibyUserId } = require('./../controllers/jenisCutiController');
-
-router.get('/', getAllJenisCuti);
-router.get("/:userid", getJenisCutibyUserId);
-
-module.exports = router;
